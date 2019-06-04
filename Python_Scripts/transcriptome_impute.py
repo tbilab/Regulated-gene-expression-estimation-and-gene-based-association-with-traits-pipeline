b25219a98f788cad667c65ddae72bab416090f86
verasiwei <siwei.zhang@vumc.org>
2019-06-04 17:17:43 -0500

    select_gene_expression

--- Python_Scripts/transcriptome_impute.py
+++ Python_Scripts/transcriptome_impute.py
@@ -19,8 +19,9 @@ os.system("module load GCC/6.4.0-2.28 Python/3.6.3")
 
 # prepare genotype file,convert plink format to dosage
 # # firstly, separate the raw genotype data into each chromosome
-for chr in range(1, 23):
-    os.system("%s --bfile %s --noweb --chr %s --make-bed --out %schr%s" % (plink, rawgenotype, chr, output, chr))
+def sepchr():
+    for chr in range(1,23):
+        os.system("%s --bfile %s --noweb --chr %s --make-bed --out %schr%s" % (plink,rawgenotype,chr,output,chr))
 
 # # parallel the conversion process
 
@@ -34,17 +35,40 @@ def multicore():
     pool.map(conversion, range(1, 23))
 
 
-multicore()
-
 # prediction genetic expression based on whole blood prediction model
-os.system(".%sPrediXcan.py --predict --dosages %s --samples %ssamples.txt --weights %s --output_prefix %s" % (pythonscript, output, output, weights, outputprefix))
+def prediction():
+    os.system(".%sPrediXcan.py --predict --dosages %s --samples %ssamples.txt --weights %s --output_prefix %s" % (pythonscript, output, output, weights, outputprefix))
+
 
 # association with phenotypes
 #association, logistic regression
 ##due to the reason of permission denied caused by subprocess.call()
 ##os.system("python %sPrediXcan.py --assoc --pheno %sjak2_pheno --missing-phenotype -9 --pred_exp %sMEGA_predicted_expression.txt --logistic --output_prefix %sMEGA_wholeblood" % (rawpath, output, result, result))
-os.system("Rscript %sPrediXcanAssociation.R PRED_EXP_FILE %sMEGA_predicted_expression.txt PHENO_FILE %s MISSING_PHENOTYPE -9 TEST_TYPE logistic OUT %s_association.txt PHENO_COLUMN None PHENO_NAME None FILTER_FILE None FILTER_VAL None FILTER_COLUMN None" % (rscript, result, phenofile, outputprefix))
+def association():
+    os.system("Rscript %sPrediXcanAssociation.R PRED_EXP_FILE %sMEGA_predicted_expression.txt PHENO_FILE %s MISSING_PHENOTYPE -9 TEST_TYPE logistic OUT %s_association.txt PHENO_COLUMN None PHENO_NAME None FILTER_FILE None FILTER_VAL None FILTER_COLUMN None" % (rscript, result, phenofile, outputprefix))
 
 # plot
 #plot and annotate the result
-os.system("Rscript %sannotate_plot.R %s 0.00005" % (rawpath, outputprefix))
+def annotate():
+    os.system("Rscript %sannotate_plot.R %s 0.00005" % (rawpath, outputprefix))
+
+#select a gene expression data by gene name from the individual level prediction data
+def selectgene(outputfile,inputfile,ncolumn_gene):
+    writePLGRKT = open(outputfile,"w")
+    with open(inputfile) as Predictmodel:
+        for line in Predictmodel:
+            sline= line.split("\t")
+            FID = sline[0]
+            IID = sline[1]
+            gene = sline[ncolumn_gene]
+            writePLGRKT.write(FID + "\t" + IID + "\t" + gene + "\n")
+    writePLGRKT.close()
+
+
+
+#sepchr()
+#multicore()
+#prediction()
+#association()
+#annotate()
+#selectgene(outputfile,inputfile,ncolumn_gene)
